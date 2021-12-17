f39ac2c8d6c10d3d71e7ebd4606d17d8c8e54c90
-=Oktan=- <r.zhigalenko@mail.ru>
2021-12-18 01:24:05 +0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QWebEngineView>
+#include <QLineEdit>
+
+
+int main(int argc, char**argv)
+{
+    QApplication app(argc, argv);
+
+    auto mainWindow = new QWidget(nullptr);
+
+    auto vBox = new QVBoxLayout(mainWindow);
+
+    auto lineEdit = new QLineEdit;
+    auto webView = new QWebEngineView;
+
+    webView->setUrl(QUrl("http://skillbox.ru"));
+
+    auto button = new QPushButton("Test");
+    auto button2 = new QPushButton("Test2");
+
+    vBox->addWidget(lineEdit);
+    vBox->addWidget(webView);
+    vBox->addWidget(button);
+    vBox->addWidget(button2);
+
+    QWidget::connect(lineEdit, &QLineEdit::returnPressed, [lineEdit, webView](){
+        webView->load(lineEdit->text());
+    });
+
+    QWidget::connect(button, &QPushButton::clicked, [&mainWindow](){
+        mainWindow->setWindowTitle("Oktan");
+    });
+
+    QWidget::connect(button2, &QPushButton::clicked, [&mainWindow](){
+        mainWindow->setWindowTitle("-=Oktan=-");
+    });
+
+    mainWindow->show();
+
+    
+
+    return app.exec();
+}
